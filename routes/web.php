164d6d54d497b4a9b564574469c0ddcc0e485267
destaptri <dstaptrii@gmail.com>
2024-11-21 20:28:17 +0700

    21 nov malam

--- routes/web.php
+++ routes/web.php
@@ -10,40 +10,51 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+// Middleware untuk semua pengguna yang login
+Route::middleware(['auth'])->group(function () {
+    // Profil - dapat diakses oleh semua pengguna yang sudah login
     Route::get('/profil', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profil', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profil', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
+// Middleware untuk role alumni
+Route::middleware(['auth', 'role:Alumni'])->group(function () {
+    // Biodata - hanya dapat diakses oleh role alumni
     Route::get('/biodata', function () {
         return view('biodata.biodata');
     })->name('biodata');
 });
 
+// Middleware untuk role admin
+Route::middleware(['auth', 'role:Admin'])->group(function () {
+    // Pencarian data - hanya dapat diakses oleh role admin
+
+    Route::get('/pencarian-data', function () {
+        return view('admin.pencarian-data');
+    })->name('pencarian-data');
+
+    Route::get('/detail-data-alumni', function () {
+        return view('admin.detail-data');
+    })->name('detail-data');
+    
+    Route::get('/validasi', function () {
+        return view('admin.validasi');
+    })->name('antrian-validasi');
+});
+
+// Route lainnya yang tidak terbatas pada role tertentu
 Route::get('/beranda', function () {
     return view('beranda');
 })->name('beranda');
-
-Route::get('/detail-data-alumni', function () {
-    return view('admin.detail-data');
-})->name('detail-data');
 
 Route::get('/hasil-pencarian', function () {
     return view('guest.hasil-pencarian');
 })->name('hasil-pencarian');
 
-Route::get('/pencarian-data', function () {
-    return view('admin.pencarian-data');
-})->name('pencarian-data');
-
-Route::get('/validasi', function () {
-    return view('admin.validasi');
-})->name('antrian-validasi');
-
-
+// Route Login
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
